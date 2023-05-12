b3ea43ff990b12f73dd04945cead8ff511a8d989
Luca Pisciotta <luca.pisciotta@lptech.io>
2023-05-12 15:18:01 +0200

    Cleanup main from provider configuration

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.55"
-    }
-  }
-  required_version = "~> 1.3"
-}
-
 data "aws_caller_identity" "current" {}
 data "aws_partition" "current" {}
 data "aws_region" "current" {}
